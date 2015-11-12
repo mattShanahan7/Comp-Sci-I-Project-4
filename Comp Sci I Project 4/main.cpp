bce65a06a175004dcc4c420fd116af64f135baf7
Matthew <Matthew@Matts-MacBook-Pro-4.local>
2015-11-11 20:37:11 -0500

    Initial Commit

---
+++ Comp Sci I Project 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Comp Sci I Project 4
+//
+//  Created by Matthew on 11/11/15.
+//  Copyright (c) 2015 Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
